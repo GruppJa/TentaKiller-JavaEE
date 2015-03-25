825911bc0329399e16cd7b5dbcc8da45a8b7f395
Hannes Landstedt <git@zarac.se>
2015-03-25 16:33:53 +0100

    Servlet

---
+++ Servlet/src/se/compmode/tentakiller/servlets/Servlet.java
@@ -0,0 +1,71 @@
+package se.compmode.tentakiller.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HEELO? service");
+		PrintWriter out = response.getWriter();
+		out.println("there is nothing (service)");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    System.out.println("HEELO? doGet");
+        PrintWriter out = response.getWriter();
+        out.println("there is nothing (doGet)");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HEELO? doPost");
+        PrintWriter out = response.getWriter();
+        out.println("there is nothing (doPost)");
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HEELO? doPut");
+        PrintWriter out = response.getWriter();
+        out.println("there is nothing (doPut)");
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HEELO? doDelete");
+        PrintWriter out = response.getWriter();
+        out.println("there is nothing (doDelete)");
+	}
+}
