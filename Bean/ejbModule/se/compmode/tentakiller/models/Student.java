535941408b70d470e9a0b99bfdb5e74ffb2c1276
Hannes Landstedt <git@zarac.se>
2015-04-08 14:59:44 +0200

    use mssql datasource, changed table structure to match that of the TentaKiller.NET program.

--- Bean/ejbModule/se/compmode/tentakiller/models/Student.java
+++ Bean/ejbModule/se/compmode/tentakiller/models/Student.java
@@ -1,5 +1,8 @@
 package se.compmode.tentakiller.models;
 
+import java.util.Date;
+import java.util.Calendar;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,8 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @NamedQueries({
@@ -16,14 +21,19 @@ import javax.persistence.Table;
     @NamedQuery(name="Student.findByEmail", query="SELECT s FROM Student s WHERE s.email LIKE :email"),
 })
 @SequenceGenerator(name="ID_SEQUENCE", allocationSize=1)
-@Table(name="Student")
+@Table(name="Students")
 public class Student {
     private long id;
     private String name;
     private String password;
     private String email;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
 
-    @Column(name="email", nullable=false, unique=true)
+    public Student() {
+        created = Calendar.getInstance().getTime(); }
+
+    @Column(name="Email", nullable=false, unique=true)
     public String getEmail() {
         return email; }
 
@@ -32,23 +42,30 @@ public class Student {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    @Column(name="id")
+    @Column(name="Id")
     public long getId() {
         return id; }
 
     public void setId(long id) {
         this.id = id; }
 
-    @Column(name="name")
+    @Column(name="Name")
     public String getName() {
         return name; }
 
     public void setName(String name) {
         this.name = name; }
 
-    @Column(name="password")
+    @Column(name="Password")
     public String getPassword() {
         return password; }
 
     public void setPassword(String password) {
-        this.password = password; } }
+        this.password = password; } 
+
+    @Column(name="Created")
+    public Date getCreated() {
+        return created; }
+
+    public void setCreated(Date created) {
+        this.created = created; } }
